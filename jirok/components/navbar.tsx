34d4aefdc0005a4df016084576733145c2435b96
Evgeniia K <fluffylion2013@gmail.com>
2026-04-07 17:42:09 +0200

    tried to install dependencies

---
+++ jirok/components/navbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Navbar() {
+
+    return (
+        <header>
+            <div>
+                <div>
+                    <span>Jirok</span>
+                </div>
+            </div>
+        </header>
+    );
+}
