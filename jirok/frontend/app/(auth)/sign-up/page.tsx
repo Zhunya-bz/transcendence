c807a984d6dc6ec8f8da23428530375bb738b6c7
zeka <zeka@Trans422.myguest.virtualbox.org>
2026-06-04 17:04:01 +0200

    remove unused variable

--- jirok/frontend/app/(auth)/sign-up/page.tsx
+++ jirok/frontend/app/(auth)/sign-up/page.tsx
@@ -39,7 +39,6 @@ const formSchema = z.object({
 
 export default function SignUp() {
   const router = useRouter();
-  const BACKEND = process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:3001";
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
