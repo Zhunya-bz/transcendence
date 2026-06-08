f2bf3de33de664bbbbd41983fd9b230630e5b939
Felix Moses <felix@tacocat.com>
2026-06-08 09:19:06 +0200

    fix nginx for school computers

--- jirok/frontend/app/(auth)/sign-in/page.tsx
+++ jirok/frontend/app/(auth)/sign-in/page.tsx
@@ -122,21 +122,21 @@ export default function SignIn() {
             or login with
           </div>
           <CardContent className="px-7">
-              <Button
-                variant="ghost"
-                className="w-full bg-orange-300 hover:bg-orange-400 mb-3"
-                onClick={() => {
-                  window.location.href = "/api/auth/42";
-                }}
-              >
-                <Image
-                  src="/42icon.svg"
-                  width={500}
-                  height={500}
-                  alt="42 Icon"
-                  className="w-[30px]"
-                />
-              </Button>
+            <Button
+              variant="ghost"
+              className="w-full bg-orange-300 hover:bg-orange-400 mb-3"
+              onClick={() => {
+                window.location.href = "/api/auth/42";
+              }}
+            >
+              <Image
+                src="/42icon.svg"
+                width={500}
+                height={500}
+                alt="42 Icon"
+                className="w-[30px]"
+              />
+            </Button>
           </CardContent>
           <div className="px-7">
             <Separator className="bg-gray-400" />
