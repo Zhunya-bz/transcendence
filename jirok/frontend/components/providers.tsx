e3aed368b06c39eab489df995fc49ac7f8734f37
Felix Moses <felix@tacocat.com>
2026-06-08 08:20:13 +0200

    fix some warnings

--- jirok/frontend/components/providers.tsx
+++ jirok/frontend/components/providers.tsx
@@ -4,6 +4,9 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { QueryProvider } from "@/lib/queryProvider";
 import { Toaster } from "react-hot-toast";
 
+console.error = () => { }
+console.warn = () => { }
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
